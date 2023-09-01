b90dabc74ea6e7ace79a6c3c02f1233fd651a514
markusVJH <markus.heinila@pp.inet.fi>
2023-09-01 14:05:35 +0300

    basic setup for productsSlice complete

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -1,7 +1,35 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const api = "https://fakrstoreapi.com/products";
 
-const InitialState = {
+const initialState = {
   products: [],
 }
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const response = await axios.get(api);
+    const data = await response.data;
+    return data;
+  }
+)
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState,
+
+  reducers: {
+    
+  },
+  extraReducers: (builder) => {
+    builder
+        .addCase(fetchProducts.fulfilled, (state, action) => {
+          state.products = action.payload;
+        })
+        .addCase(fetchProducts.rejected, (state, action) => {
+          state.products = [];
+        })
+  }
+})
